ce017007daf07eb5d62fa117d145aaef9e792f82
Steve <34465153+xxl4@users.noreply.github.com>
2024-09-12 15:17:38 +0800

    fix add route

--- src/Routes/admin.php
+++ src/Routes/admin.php
@@ -9,6 +9,7 @@
  */
 use Illuminate\Support\Facades\Route;
 use NexaMerchant\GoogleAdsense\Http\Controllers\Admin\ExampleController;
+use NexaMerchant\GoogleAdsense\Http\Controllers\Admin\GoogleAdsenseController;
 
 Route::group(['middleware' => ['admin','admin_option_log'], 'prefix' => config('app.admin_url')], function () {
     Route::prefix('googleadsense')->group(function () {
@@ -18,6 +19,22 @@ Route::group(['middleware' => ['admin','admin_option_log'], 'prefix' => config('
             Route::get('demo', 'demo')->name('googleadsense.admin.example.demo');
 
         });
+
+        // Google Adsense
+        Route::get('/google/adsense/accounts/list', [GoogleAdsenseController::class, 'AccountsList']);
+        Route::get('/google/adsense/accounts/get', [GoogleAdsenseController::class, 'AccountsGet']);
+
+        Route::get('/google/adsense/accounts/adclients/list', [GoogleAdsenseController::class, 'AccountsAdclientsList']);
+        Route::get('/google/adsense/accounts/adclients/get', [GoogleAdsenseController::class, 'AccountsAdclientsGet']);
+
+        Route::get('/google/adsense/accounts/adclients/adunits/list', [GoogleAdsenseController::class, 'AccountsAdclientsAdunitsList']);
+        Route::get('/google/adsense/accounts/adclients/adunits/get', [GoogleAdsenseController::class, 'AccountsAdclientsAdunitsGet']);
+        Route::get('/google/adsense/accounts/adclients/adunits/get-ad-code', [GoogleAdsenseController::class, 'AccountsAdclientsAdunitsGetAdCode']);
+        
+        Route::get('/google/adsense/accounts/adclients/adunits/custom-channels/list', [GoogleAdsenseController::class, 'AccountsAdclientsAdunitsCustomChannelsList']);
+
+        // Account Reports Generate
+        Route::get('/google/adsense/accounts/reports/generate', [GoogleAdsenseController::class, 'AccountsReportsGenerate']);
 
     });
 });
